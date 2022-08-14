ce45ec3b55c2f8e280bbb10608f98517be99e484
ritika <ritikasinghal2101@gmail.com>
2022-08-14 16:06:51 +0530

    last

--- src/Share.js
+++ src/Share.js
@@ -1,6 +1,6 @@
-import React from 'react'
+// import React from 'react'
 import Nav from './Nav'
-import { Link , Outlet} from 'react-router-dom'
+import { Outlet} from 'react-router-dom'
 function Share() {
   return (<>
   <Nav/>
